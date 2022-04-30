549be172fea3e5585e99e86b01eb82126998eabf
whyyougd <holygd73@naver.com>
2022-04-30 11:58:21 +0900

    first commit

---
+++ DaddyChat/src/main/java/com/example/daddychat/DaddyChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.daddychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaddyChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaddyChatApplication.class, args);
+    }
+
+}
---
+++ DaddyChat/src/test/java/com/example/daddychat/DaddyChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daddychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaddyChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
